0c7201c2a979e43f3e118aadad4034335eeef1b2
Micah Veith <101129100@student.swin.edu.au>
2021-09-14 18:39:32 +1000

    Added coordinates to chess piece. Added get available moves function. Updates to coordinate structure and various enums.

--- Library/ChessPiece.cpp
+++ Library/ChessPiece.cpp
@@ -1,11 +1,9 @@
 #include "ChessPiece.h"
 
-// Base Chess Class
-ChessPiece::ChessPiece() {
-}
-
-ChessPiece::ChessPiece(COLOUR newColour) {
+ChessPiece::ChessPiece(COLOUR newColour, COORDINATE newCoordinate) {
 	myColour = newColour;
+	myCoordinate = newCoordinate;
+	myPieceType = PIECETYPE::NONE;
 }
 
 COLOUR ChessPiece::getColour() {
@@ -16,60 +14,115 @@ PIECETYPE ChessPiece::getPieceType() {
 	return myPieceType;
 }
 
-char ChessPiece::getPieceChar() {
-	return ' ';
+//char ChessPiece::getPieceChar() {
+//	return ' ';
+//}
+
+//void ChessPiece::getAvailableMoves(BOOLBOARD board) {
+//	;
+//}
+
+void ChessPiece::setCoordinate(COORDINATE newCoordinate) {
+	myCoordinate = newCoordinate;
+}
+
+COORDINATE ChessPiece::getCoordinate() {
+	return myCoordinate;
 }
 
 // Pawn
-Pawn::Pawn(COLOUR newColour) : ChessPiece(newColour) {
-	myPieceType = PAWN;
+Pawn::Pawn(COLOUR newColour, COORDINATE newCoordinate) : ChessPiece(newColour, newCoordinate) {
+	myPieceType = PIECETYPE::PAWN;
 }
 
 char Pawn::getPieceChar() {
 	return 'P';
 }
 
+void Pawn::getAvailableMoves(BOOLBOARD* board) {
+	
+	switch (myColour) {
+		case COLOUR::BLACK:
+			if (myCoordinate.Y == 7) {
+				board->board[myCoordinate.X][myCoordinate.Y - 1] = true;
+				board->board[myCoordinate.X][myCoordinate.Y - 2] = true;
+			}
+			else if (myCoordinate.Y > 1) {
+				board->board[myCoordinate.X][myCoordinate.Y - 1] = true;
+			}
+		case COLOUR::WHITE:
+			if (myCoordinate.Y == 2) {
+				board->board[myCoordinate.X][myCoordinate.Y + 1] = true;
+				board->board[myCoordinate.X][myCoordinate.Y + 2] = true;
+			}
+			else if	(myCoordinate.Y < 8) {
+				board->board[myCoordinate.X][myCoordinate.Y + 1] = true;
+			}
+		}
+		
+}
+
 // Rook
-Rook::Rook(COLOUR newColour) : ChessPiece(newColour) {
-	myPieceType = ROOK;
+Rook::Rook(COLOUR newColour, COORDINATE newCoordinate) : ChessPiece(newColour, newCoordinate) {
+	myPieceType = PIECETYPE::ROOK;
 }
 
 char Rook::getPieceChar() {
 	return 'R';
 }
 
+void Rook::getAvailableMoves(BOOLBOARD* board)
+{
+}
+
 // Knight
-Knight::Knight(COLOUR newColour) : ChessPiece(newColour) {
-	myPieceType = KNIGHT;
+Knight::Knight(COLOUR newColour, COORDINATE newCoordinate) : ChessPiece(newColour, newCoordinate) {
+	myPieceType = PIECETYPE::KNIGHT;
 }
 
 char Knight::getPieceChar() {
 	return 'K';
 }
 
+void Knight::getAvailableMoves(BOOLBOARD* board)
+{
+}
+
 // Bishop
-Bishop::Bishop(COLOUR newColour) : ChessPiece(newColour) {
-	myPieceType = BISHOP;
+Bishop::Bishop(COLOUR newColour, COORDINATE newCoordinate) : ChessPiece(newColour, newCoordinate) {
+	myPieceType = PIECETYPE::BISHOP;
 }
 
 char Bishop::getPieceChar() {
 	return 'B';
 }
 
+void Bishop::getAvailableMoves(BOOLBOARD* board)
+{
+}
+
 // Queen
-Queen::Queen(COLOUR newColour) : ChessPiece(newColour) {
-	myPieceType = QUEEN;
+Queen::Queen(COLOUR newColour, COORDINATE newCoordinate) : ChessPiece(newColour, newCoordinate) {
+	myPieceType = PIECETYPE::QUEEN;
 }
 
 char Queen::getPieceChar() {
 	return 'Q';
 }
 
+void Queen::getAvailableMoves(BOOLBOARD* board)
+{
+}
+
 // King
-King::King(COLOUR newColour) : ChessPiece(newColour) {
-	myPieceType = KING;
+King::King(COLOUR newColour, COORDINATE newCoordinate) : ChessPiece(newColour, newCoordinate) {
+	myPieceType = PIECETYPE::KING;
 }
 
 char King::getPieceChar() {
 	return 'K';
+}
+
+void King::getAvailableMoves(BOOLBOARD* board)
+{
 }
--- Library/ChessPiece.h
+++ Library/ChessPiece.h
@@ -1,67 +1,84 @@
 #pragma once
 
-// Chess Piece Types
-enum PIECETYPE {PAWN = 1, ROOK, KNIGHT, BISHOP, QUEEN, KING};
-enum ALPHANUM {A = 1, B, C, D, E, F, G, H};
-enum COLOUR {WHITE = 0, BLACK = 1};
+// Chess Piece Types & Colours
+enum class PIECETYPE {NONE = 0, PAWN, ROOK, KNIGHT, BISHOP, QUEEN, KING};\
+enum class COLOUR {WHITE = 0, BLACK = 1};
+enum ALPHANUM { NONE = 0, A, B, C, D, E, F, G, H };
 
+// Coordinate Structure
 struct COORDINATE {
 	ALPHANUM X;
 	int Y;
+	bool operator < (const COORDINATE rhs) const { return (this->X < rhs.X) || ((this->X == rhs.X) && (this->Y < rhs.Y)); }
+	bool operator > (const COORDINATE rhs) const { return (this->X > rhs.X) || ((this->X == rhs.X) && (this->Y > rhs.Y)); }
+};
+
+// Array of bools
+struct BOOLBOARD {
+	bool board[H + 1][8 + 1];
 };
 
 // Base Chess Piece Class
 class ChessPiece {
 	protected:
-		COORDINATE myCoordinate;
 		PIECETYPE myPieceType;
 		COLOUR myColour;
+		COORDINATE myCoordinate;
 	public:
-		ChessPiece();
-		ChessPiece(COLOUR newColour);
+		//ChessPiece();
+		ChessPiece(COLOUR newColour, COORDINATE newCoordinate);
 		COLOUR getColour();
 		PIECETYPE getPieceType();
-		virtual char getPieceChar();
+		virtual char getPieceChar() = 0;
+		virtual void getAvailableMoves(BOOLBOARD* board) = 0;
+		void setCoordinate(COORDINATE newCoordinate);
+		COORDINATE getCoordinate();
 };
 
 // Pawn
 class Pawn : public ChessPiece {
 	public:
-		Pawn(COLOUR newColour);
+		Pawn(COLOUR newColour, COORDINATE newCoordinate);
 		char getPieceChar();
+		void getAvailableMoves(BOOLBOARD* board);
 };
 
 // Rook
 class Rook : public ChessPiece {
 public:
-	Rook(COLOUR newColour);
+	Rook(COLOUR newColour, COORDINATE newCoordinate);
 	char getPieceChar();
+	void getAvailableMoves(BOOLBOARD* board);
 };
 
 // Knight
 class Knight : public ChessPiece {
 public:
-	Knight(COLOUR newColour);
+	Knight(COLOUR newColour, COORDINATE newCoordinate);
 	char getPieceChar();
+	void getAvailableMoves(BOOLBOARD* board);
 };
 
 // Bishop
 class Bishop : public ChessPiece {
 public:
-	Bishop(COLOUR newColour);
+	Bishop(COLOUR newColour, COORDINATE newCoordinate);
 	char getPieceChar();
+	void getAvailableMoves(BOOLBOARD* board);
 };
 
 // Queen
 class Queen : public ChessPiece {
 public:
-	Queen(COLOUR newColour);
+	Queen(COLOUR newColour, COORDINATE newCoordinate);
 	char getPieceChar();
+	void getAvailableMoves(BOOLBOARD* board);
 };
 
 // King
 class King : public ChessPiece {
 public:
-	King(COLOUR newColour);
+	King(COLOUR newColour, COORDINATE newCoordinate);
 	char getPieceChar();
+	void getAvailableMoves(BOOLBOARD* board);
 };
