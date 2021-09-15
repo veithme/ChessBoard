cfbb3ce9e6ef98867c8370351816782abb5599c6
Micah Veith <101129100@student.swin.edu.au>
2021-09-15 12:23:09 +1000

    Added move validation Modified valid move array structure

--- Library/ChessBoard.cpp
+++ Library/ChessBoard.cpp
@@ -1,3 +1,4 @@
+#include "ChessPiece.h"
 #include "ChessBoard.h"
 #include <windows.h>
 
@@ -16,58 +17,54 @@ using namespace std;
 */
 ChessBoard::ChessBoard() {
 
-	myBoard[{A,1}] = new Rook  (COLOUR::WHITE, { A, 1 });
-	myBoard[{B,1}] = new Knight(COLOUR::WHITE, { B, 1 });
-	myBoard[{C,1}] = new Bishop(COLOUR::WHITE, { C, 1 });
-	myBoard[{D,1}] = new King(COLOUR::WHITE, { D, 1 });
-	myBoard[{E,1}] = new Queen(COLOUR::WHITE, { E, 1 });
-	myBoard[{F,1}] = new Bishop(COLOUR::WHITE, { F, 1 });
-	myBoard[{G,1}] = new Knight(COLOUR::WHITE, { G, 1 });
-	myBoard[{H,1}] = new Rook(COLOUR::WHITE, { H, 1 });
+	myBoard[{A,1}] = new Rook  (COLOUR::WHITE, A, 1);
+	myBoard[{B,1}] = new Knight(COLOUR::WHITE, B, 1);
+	myBoard[{C,1}] = new Bishop(COLOUR::WHITE, C, 1);
+	myBoard[{D,1}] = new King(COLOUR::WHITE, D, 1);
+	myBoard[{E,1}] = new Queen(COLOUR::WHITE, E, 1);
+	myBoard[{F,1}] = new Bishop(COLOUR::WHITE, F, 1);
+	myBoard[{G,1}] = new Knight(COLOUR::WHITE, G, 1);
+	myBoard[{H,1}] = new Rook(COLOUR::WHITE, H, 1);
 
-	myBoard[{A,2}] = new Pawn(COLOUR::WHITE, { A, 2 });
-	myBoard[{B,2}] = new Pawn(COLOUR::WHITE, { B, 2 });
-	myBoard[{C,2}] = new Pawn(COLOUR::WHITE, { C, 2 });
-	myBoard[{D,2}] = new Pawn(COLOUR::WHITE, { D, 2 });
-	myBoard[{E,2}] = new Pawn(COLOUR::WHITE, { E, 2 });
-	myBoard[{F,2}] = new Pawn(COLOUR::WHITE, { F, 2 });
-	myBoard[{G,2}] = new Pawn(COLOUR::WHITE, { G, 2 });
-	myBoard[{H,2}] = new Pawn(COLOUR::WHITE, { H, 2 });
+	myBoard[{A,2}] = new Pawn(COLOUR::WHITE, A, 2);
+	myBoard[{B,2}] = new Pawn(COLOUR::WHITE, B, 2);
+	myBoard[{C,2}] = new Pawn(COLOUR::WHITE, C, 2);
+	myBoard[{D,2}] = new Pawn(COLOUR::WHITE, D, 2);
+	myBoard[{E,2}] = new Pawn(COLOUR::WHITE, E, 2);
+	myBoard[{F,2}] = new Pawn(COLOUR::WHITE, F, 2);
+	myBoard[{G,2}] = new Pawn(COLOUR::WHITE, G, 2);
+	myBoard[{H,2}] = new Pawn(COLOUR::WHITE, H, 2);
 
-	myBoard[{A,8}] = new Rook  (COLOUR::BLACK, { A, 8 });
-	myBoard[{B,8}] = new Knight(COLOUR::BLACK, { B, 8 });
-	myBoard[{C,8}] = new Bishop(COLOUR::BLACK, { C, 8 });
-	myBoard[{D,8}] = new King(COLOUR::BLACK, { D, 8 });
-	myBoard[{E,8}] = new Queen(COLOUR::BLACK, { E, 8 });
-	myBoard[{F,8}] = new Bishop(COLOUR::BLACK, { F, 8 });
-	myBoard[{G,8}] = new Knight(COLOUR::BLACK, { G, 8 });
-	myBoard[{H,8}] = new Rook(COLOUR::BLACK, { H, 8 });
+	myBoard[{A,8}] = new Rook  (COLOUR::BLACK, A, 8);
+	myBoard[{B,8}] = new Knight(COLOUR::BLACK, B, 8);
+	myBoard[{C,8}] = new Bishop(COLOUR::BLACK, C, 8);
+	myBoard[{D,8}] = new King(COLOUR::BLACK, D, 8);
+	myBoard[{E,8}] = new Queen(COLOUR::BLACK, E, 8);
+	myBoard[{F,8}] = new Bishop(COLOUR::BLACK, F, 8);
+	myBoard[{G,8}] = new Knight(COLOUR::BLACK, G, 8);
+	myBoard[{H,8}] = new Rook(COLOUR::BLACK, H, 8);
 
-	myBoard[{A,7}] = new Pawn(COLOUR::BLACK, { A, 7 });
-	myBoard[{B,7}] = new Pawn(COLOUR::BLACK, { B, 7 });
-	myBoard[{C,7}] = new Pawn(COLOUR::BLACK, { C, 7 });
-	myBoard[{D,7}] = new Pawn(COLOUR::BLACK, { D, 7 });
-	myBoard[{E,7}] = new Pawn(COLOUR::BLACK, { E, 7 });
-	myBoard[{F,7}] = new Pawn(COLOUR::BLACK, { F, 7 });
-	myBoard[{G,7}] = new Pawn(COLOUR::BLACK, { G, 7 });
-	myBoard[{H,7}] = new Pawn(COLOUR::BLACK, { H, 7 });
+	myBoard[{A,7}] = new Pawn(COLOUR::BLACK, A, 7);
+	myBoard[{B,7}] = new Pawn(COLOUR::BLACK, B, 7);
+	myBoard[{C,7}] = new Pawn(COLOUR::BLACK, C, 7);
+	myBoard[{D,7}] = new Pawn(COLOUR::BLACK, D, 7);
+	myBoard[{E,7}] = new Pawn(COLOUR::BLACK, E, 7);
+	myBoard[{F,7}] = new Pawn(COLOUR::BLACK, F, 7);
+	myBoard[{G,7}] = new Pawn(COLOUR::BLACK, G, 7);
+	myBoard[{H,7}] = new Pawn(COLOUR::BLACK, H, 7);
+
+	validMoves = new BOOLBOARD{ false };
 
 }
 
 void ChessBoard::printBoard() {
 
 	ChessPiece* piece;
-	BOOLBOARD validMoveBoard = { false };
 	
 	// Color of the console text
 	HANDLE console_color;
 	console_color = GetStdHandle(STD_OUTPUT_HANDLE);
 	SetConsoleTextAttribute(console_color, 8);
-
-	// Get valid moves from selected piece
-	if (selectedPiece != nullptr) {
-		selectedPiece->getAvailableMoves(&validMoveBoard);
-	}
 
 	// Loop through all tiles of chess board
 	cout << endl;
@@ -86,15 +83,15 @@ void ChessBoard::printBoard() {
 					SetConsoleTextAttribute(console_color, 10); // Green
 				}
 				else if (piece->getColour() == COLOUR::BLACK) {
-					SetConsoleTextAttribute(console_color, (validMoveBoard.board[x][y] == true ? 41 : 9)); // Blue
+					SetConsoleTextAttribute(console_color, (validMoves->arr[x][y] == true ? 41 : 9)); // Blue
 				}
 				else {
-					SetConsoleTextAttribute(console_color, (validMoveBoard.board[x][y] == true ? 47 : 15)); // White
+					SetConsoleTextAttribute(console_color, (validMoves->arr[x][y] == true ? 47 : 15)); // White
 				}
 				cout << piece->getPieceChar();
 			}
 			else {
-				SetConsoleTextAttribute(console_color, (validMoveBoard.board[x][y] == true ? 47 : 15)); // White
+				SetConsoleTextAttribute(console_color, (validMoves->arr[x][y] == true ? 47 : 15)); // White
 				cout << " ";
 			}
 
@@ -110,9 +107,21 @@ void ChessBoard::printBoard() {
 }
 
 bool ChessBoard::movePiece(ChessPiece* piece, COORDINATE newCoordinate) {
-	myBoard[piece->getCoordinate()] = nullptr;
-	myBoard[newCoordinate] = piece;
-	piece->setCoordinate(newCoordinate);
+	// Check move is valid
+	if (validMoves->arr[newCoordinate.X][newCoordinate.Y]) {
+		myBoard[piece->getCoordinate()] = nullptr;
+		delete myBoard[newCoordinate];
+		myBoard[newCoordinate] = piece;
+		piece->setCoordinate(newCoordinate);
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+ChessPiece* ChessBoard::getPiece(COORDINATE coordinate){
+	return myBoard[coordinate];
 }
 
 ChessPiece* ChessBoard::selectPiece(COORDINATE coordinate, COLOUR colour) {
@@ -127,4 +136,13 @@ ChessPiece* ChessBoard::selectPiece(COORDINATE coordinate, COLOUR colour) {
 
 void ChessBoard::unselectPiece() {
 	selectedPiece = nullptr;
+}
+
+void ChessBoard::checkValidMoves() {
+	selectedPiece->getAvailableMoves(validMoves, this);
+}
+
+void ChessBoard::resetValidMoves() {
+	delete validMoves;
+	validMoves = new BOOLBOARD{ false };
 }
--- Library/ChessBoard.h
+++ Library/ChessBoard.h
@@ -1,19 +1,29 @@
 #pragma once
 
-#include "ChessPiece.h"
+#include "Enum.h"
+#include "Structs.h"
+
 #include <iostream>
 #include <map>
 
 using namespace std;
 
+// Forward Declarations
+class ChessPiece;
+
+// Chess Board Class
 class ChessBoard {
 	protected:
 		map < COORDINATE, ChessPiece* > myBoard;
 		ChessPiece* selectedPiece = nullptr;
+		BOOLBOARD* validMoves;
 	public:
 		ChessBoard();
 		void printBoard();
 		bool movePiece(ChessPiece* piece, COORDINATE newCoordinate);
+		ChessPiece* getPiece(COORDINATE coordinate);
 		ChessPiece* selectPiece(COORDINATE coordinate, COLOUR colour);
 		void unselectPiece();
+		void checkValidMoves();
+		void resetValidMoves();
 };
